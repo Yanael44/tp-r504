5272097d8554c34d11cbe1854d4c80c9ee126537
Yanael44 <yanael246@gmail.com>
2026-09-08 15:30:59 +0200

    Remplacement de l'opérateur ** par une boucle for

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,12 +1,16 @@
 def puissance(a, b):
-    if not type(a) is int:
-        raise TypeError
-    if not type(b) is int:
-        raise TypeError
+    
+    if not type(a) is int or not type(b) is int:
+        raise TypeError   
     if a == 0:
         if b < 0:
-            raise ValueError
-        if b > 0:
-            return 0
+            raise ValueError 
+        return 0
+    res = 1
+    exp = abs(b)
+    for _ in range(exp):
+        res = res * a
+    if b < 0:
+        return 1 / res
 
-    return a ** b
+    return res
